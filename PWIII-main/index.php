0b86bf070edf9a5cbcca92a40f7b4064f2e700e1
netinho007 <141058539+netinho007@users.noreply.github.com>
2025-02-16 01:46:38 -0300

    ca

--- PWIII-main/index.php
+++ PWIII-main/index.php
@@ -1,35 +1,33 @@
 
 <?php include './header.php'; ?>
 
-
+   
   <?php
   class dentista {
     public $nome;
-    public $poder;
+    public $Especialidade;
     public $foto;
 
   }
-  $dentista1 = new dentista();
-  $dentista1->nome = "pikachu";
-  $dentista1->poder = "poder eletrizante";
-  $dentista1->foto = "https://img.pokemondb.net/artwork/large/pikachu.jpg";
 
+  $dentista1 = new dentista();
+  $dentista1->nome = "Pamela";
+  // $dentista1->$Especialidade = "";
+  $dentista1->foto = "fotos/dra_pamela.png"; 
+  
   $dentista2 = new dentista();
   $dentista2->nome = "Charizard";
-  $dentista2->nome = "Charizard";
-  $dentista2->poder = "Poder de fogo ardente";
+  $dentista2->Especialidade = "Poder de fogo ardente";
   $dentista2->foto = "";
 
   $dentista3 = new dentista();
   $dentista3->nome = "Charizard";
-  $dentista3->nome = "Charizard";
-  $dentista3->poder = "Poder de fogo ardente";
+  $dentista3->Especialidade = "Poder de fogo ardente";
   $dentista3->foto = "";
 
   $dentista4 = new dentista();
   $dentista4->nome = "Charizard";
-  $dentista4->nome = "Charizard";
-  $dentista4->poder = "Poder de fogo ardente";
+  $dentista4->Especialidade = "Poder de fogo ardente";
   $dentista4->foto = "";
 
     $dentistas = [
@@ -49,8 +47,37 @@
             <img src="<?php echo $dentista->foto;?>" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title"><?php echo $dentista->nome;?></h5>
-              <p class="card-text"><?php echo $dentista->poder;?></p>
-              <a href="#" class="btn btn-primary">Ver mais</a>
+              <p class="card-text"><?php echo $dentista->Especialidade;?></p>
+            <!-- Botão para abrir o modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#meuModal">
+        Ver mais
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="meuModal" tabindex="-1" aria-labelledby="meuModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="meuModalLabel">Especialidades</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+  BOA NOITE 
+</button>
+
+
             </div>
           </div>
         </div>
@@ -59,23 +86,10 @@
        
         </div>
     </div>
-    function toggleText() {
-            var info = document.getElementById("info-oculta");
-            var botao = document.getElementById("botao-toggle");
-            
-            if (info.style.display === "none") {
-                info.style.display = "block";
-                botao.innerText = "Ver menos"; // Altera o texto do botão
-            } else {
-                info.style.display = "none";
-                botao.innerText = "Ver mais";
-            }
-        }
+    
     </script>
-</head>
-<body class="p-4">
 
-    <a href="#" id="botao-toggle" class="btn btn-primary" onclick="toggleText()">Ver mais</a>
+
 
     <div id="info-oculta" style="display: none; margin-top: 15px;">
         <p>Esta é a informação oculta que será mostrada quando o botão for clicado.</p>
